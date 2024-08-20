45182c32b55d4241861333fc1354ee73bf1a9979
chirayu16 <chirayupo16@gmail.com>
2024-08-20 19:56:55 +0530

    fixed imports array

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 import { HeroComponent } from '../hero/hero.component';
 import { ProjectsComponent } from '../projects/projects.component';
 import { ContactComponent } from '../contact/contact.component';
@@ -9,7 +8,7 @@ import { NavComponent } from '../nav/nav.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, ProjectsComponent, ContactComponent, AboutComponent, NavComponent],
+  imports: [HeroComponent, ProjectsComponent, ContactComponent, AboutComponent, NavComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
