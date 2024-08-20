3e68ccb7729eb529f8864ac4cf8264e970ce3166
chirayu16 <chirayupo16@gmail.com>
2024-08-20 19:44:29 +0530

    improved design

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,27 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RedirectionService } from '../service/redirection.service';
-RedirectionService
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
 
   navItems = [
     { label: 'ABOUT', id: 'about' },
-    { label: 'CONTACT', id: 'contact' },
-    { label: 'PROJECTS', id: 'projects' }
+    { label: 'CONTACT', id: 'contact', url: 'https://form.jotform.com/242323950402447' },
+    { label: 'PROJECTS', id: 'projects' },
+    { label: 'RESUME', id: 'resume', url: 'https://drive.google.com/file/d/1-fDMymbAkpvR7OlnEaWFwxl9BxtE3nYO/view?usp=sharing' }
+    
   ];
 
-  constructor(private redirectionService : RedirectionService) {}
+  constructor(private redirectionService: RedirectionService) {}
 
-  scrollTo(sectionId: string): void {
-    this.redirectionService.scrollToSection(sectionId);
+  handleNavigation(item: { id: string, url?: string }): void {
+    if (item.url) {
+      window.open(item.url, '_blank', 'noopener,noreferrer');
+    } else {
+      this.redirectionService.scrollToSection(item.id);
+    }
   }
-
 }
