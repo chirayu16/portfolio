d58f43cb6f8f727d4a68d330064cae50307c04c8
chirayu16 <chirayupo16@gmail.com>
2024-09-20 11:34:07 +0530

    font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,10 @@ module.exports = {
       },
       boxShadow: {
         'project-card-shadow': '0px 0px 4px 2px #506486',
-      }
+      },
+      fontFamily: {
+        inter: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      },
     },
   },
   plugins: [],
