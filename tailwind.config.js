7e3e38a4cb0896b19894adbad82a5cdce232d18d
chirayu16 <chirayupo16@gmail.com>
2024-09-24 14:20:15 +0530

    mobile view fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
         "8-px":  "8px",
         "16-px": "16px",
         "24-px": "24px",
+        "36-px": "36px",
         "48-px": "48px",
         "90-px": "90px" ,
         "96-px": "96px",
@@ -26,7 +27,7 @@ module.exports = {
       },
       margin: {
         "8-px":"8px",
-        "12-px":"8px",
+        "12-px":"12px",
         "16-px":"16px",
         "24-px":"24px",
         "32-px":"32px",
