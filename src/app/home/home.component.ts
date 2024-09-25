c21c72ea76d266db7888da7ed58d780719a5b987
chirayu16 <chirayupo16@gmail.com>
2024-09-25 23:52:38 +0530

    added experience

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { ProjectsComponent } from '../projects/projects.component';
 import { ContactComponent } from '../contact/contact.component';
 import { AboutComponent } from '../about/about.component';
 import { NavComponent } from '../nav/nav.component';
+import { ExperienceComponent } from '../experience/experience.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, ProjectsComponent, ContactComponent, AboutComponent, NavComponent],
+  imports: [HeroComponent, ProjectsComponent, ContactComponent, AboutComponent, NavComponent, ExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
